41d5abadb276767af3997697d34726a626f6f68c
jan.geci <seanjoshuaronan@gmail.com>
2025-03-10 16:19:39 +0100

    fix error during login

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -28,7 +28,7 @@ class UserController extends Controller
                     'open_id' => 'required',
                     'name' => 'required',
                     'email' => 'required',
-                    'password' => 'nullable|min:6',
+//                    'password' => 'nullable|min:6',
                 ]);
 
             if ($validateUser->fails()) {
